9bb1fbc37671585024cec925800b856c243aca2f
Guillaume REYNAUD <webmaster@quick-tutoriel.com>
2025-08-09 15:52:43 +0200

    Delete check_palo_sync.pl

--- check_palo_sync.pl
+++
@@ -1,8 +0,0 @@
-# Plugin-Palo-Alto-Check-HA-Sync
-Supervising Palo Alto HA synchronisation via REST API: Perl script for Icinga/Nagios (https://quick-tutoriel.com/superviser-la-synchronisation-ha-de-palo-alto/)
-
-Monitoring Palo Alto HA synchronisation is a key step in ensuring the reliability, consistency, and security of your network infrastructure. Using the REST API and the Palo Alto API key, it is possible to automate this monitoring using a dedicated Perl script designed to check the synchronisation status between HA cluster members.
-
-This script facilitates Palo Alto monitoring by quickly identifying configuration desynchronisations. It can be easily integrated into monitoring tools such as Icinga or Centreon, offering a simple, effective and adaptable solution to strengthen your high availability policies.
-
-
